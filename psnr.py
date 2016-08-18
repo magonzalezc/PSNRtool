5b45cf20b0b4fd65c2d8c5c3f285d1df53b790a1
Marina Gonzalez <magonzalezc@dit.upm.es>
2016-08-18 13:53:48 +0200

    Minor changes

--- psnr.py
+++ psnr.py
@@ -17,37 +17,37 @@ import math
 
 def calculatePSNR(original_y, encoded_y, original_cb, encoded_cb, original_cr, encoded_cr, npix):
 
-        total_y = 0 # Summatory of y squared errors
-	total_cb = 0 # Summatory of cr squared errors
-	total_cr = 0 # Summatory of cb squared errors
+        error_y = 0 # Summatory of y squared errors
+	error_cb = 0 # Summatory of cr squared errors
+	error_cr = 0 # Summatory of cb squared errors
         for i in range(0, len(original_y)): 
                 
-                dif_y = encoded_y[i] - original_y[i] # Simple error between predicted and original luminance
-		dif_cb = encoded_cb[i] - original_cb[i] # Simple error between predicted and original cb
-		dif_cr = encoded_cr[i] - original_cr[i] # Simple error between predicted and original cr
-                total_y = total_y + pow(dif_y, 2) # We add y square to the summatory
-		total_cb = total_cb + pow(dif_cb,2) # We add cb square to the summatory
-		total_cr = total_cr + pow(dif_cr,2) # We add cr square to the summatory
+                dif_y = abs(original_y[i] - encoded_y[i])    # Simple error between predicted and original luminance
+		dif_cb = abs(original_cb[i] - encoded_cb[i]) # Simple error between predicted and original cb
+		dif_cr = abs(original_cr[i] - encoded_cr[i]) # Simple error between predicted and original cr
+                error_y += dif_y*dif_y    # We add y square to the summatory
+		error_cb += dif_cb*dif_cb # We add cb square to the summatory
+		error_cr += dif_cr*dif_cr # We add cr square to the summatory
                          
-        meanSquaredErrorY = float(total_y) / float(npix) # And we get the mean squared error per pixel
-        meanSquaredErrorCb = float(total_cb) / float(npix) # And we get the mean squared error per pixel
-        meanSquaredErrorCr = float(total_cr) / float(npix) # And we get the mean squared error per pixel
+        meanSquaredErrorY = float(error_y) / float(npix) # And we get the mean squared error per pixel
+        meanSquaredErrorCb = float(error_cb) / float(npix) # And we get the mean squared error per pixel
+        meanSquaredErrorCr = float(error_cr) / float(npix) # And we get the mean squared error per pixel
        
 	if (meanSquaredErrorY != 0):
 		# We use 255*255 because we're using 8 bits for every luminance value
-                peakSignalToNoiseRatioY = float(10 * math.log(255 * 255 / meanSquaredErrorY, 10)) 
+                peakSignalToNoiseRatioY = float(-10.0 * math.log(meanSquaredErrorY/(255 * 255), 10)) 
         else:
                 peakSignalToNoiseRatioY = 0
 
 	if (meanSquaredErrorCb != 0):
 		# We use 255*255 because we're using 8 bits for every luminance value
-                peakSignalToNoiseRatioCb = float(10 * math.log(255 * 255 / meanSquaredErrorCb, 10)) 
+                peakSignalToNoiseRatioCb = float(-10.0 * math.log(meanSquaredErrorCb/(255 * 255), 10)) 
         else:
                 peakSignalToNoiseRatioCb = 0
 
 	if (meanSquaredErrorCr != 0):
 		# We use 255*255 because we're using 8 bits for every luminance value
-                peakSignalToNoiseRatioCr = float(10 * math.log(255 * 255 / meanSquaredErrorCr, 10)) 
+                peakSignalToNoiseRatioCr = float(-10.0 * math.log(meanSquaredErrorCr/(255 * 255), 10)) 
         else:
                 peakSignalToNoiseRatioCr = 0
 
