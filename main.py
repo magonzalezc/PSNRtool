030bc021f1b3cea79a541459ee76c87f0ee74fbc
Marina Gonzalez <magonzalezc@dit.upm.es>
2016-09-29 17:42:39 +0200

    Changes gitignore. Other minor changes

--- main.py
+++ main.py
@@ -19,14 +19,17 @@ if __name__=='__main__':
 
 
 	original = args[0]
-	encoded_yuv = args[1]
+	encoded = args[1]
 
 	original_width, original_height, original_npix = getImageData(original)
 
 	# Compare original image with encoded image with NV12 format
 	original_r, original_g, original_b = getRGB(original, original_npix)
 	original_y, original_cb, original_cr = RGBtoYUV(original_r, original_g, original_b)
-	encoded_y, encoded_cb, encoded_cr = getYUV(encoded_yuv, original_width, original_height)
+	encoded_y, encoded_cb, encoded_cr = getYUV(encoded, original_width, original_height)
+
+	#encoded_r, encoded_g, encoded_b = getRGB(encoded, original_npix)
+	#encoded_y, encoded_cb, encoded_cr = RGBtoYUV(encoded_r, encoded_g, encoded_b)
 	
 	calculatePSNR_Y(original_y, encoded_y, original_npix)
 
